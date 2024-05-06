3b1885d9d3d8f975e82fbe72108e6ba7a8bdffb7
dmarlop <davidmarinlpz@gmail.com>
2024-05-06 16:29:55 +0200

    cambios

---
+++ src/pruebaGit/recorrerCadena.java
@@ -0,0 +1,27 @@
+package pruebaGit;
+
+public class recorrerCadena {
+
+	public static void main(String[] args) {
+		
+
+	
+
+			
+				  String cadenaEjemplo = "Un hola mundo, para variar";
+			        
+			        
+			        System.out.println("Recorriendo la cadena usando un bucle for:");
+			        for (int i = 0; i < cadenaEjemplo.length(); i++) {
+			            char caracter = cadenaEjemplo.charAt(i);
+			            System.out.println("Carácter en la posición " + i + ": " + caracter);
+			            String caracterdos = "prueba" ;
+			        }
+			}
+
+		
+
+
+	}
+
+
